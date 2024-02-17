f22bbfb9cd424afdae5ac31fe8eb0c86ec3c4a93
HsuXinYu <jolin7180732@gmail.com>
2024-02-17 17:29:02 +0800

    upload image to firebase

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -3,6 +3,7 @@ const express = require("express");
 const router = express.Router();
 const userController = require("../controllers/userController");
 const productController = require("../controllers/productController");
+const uploadController = require("../controllers/uploadController");
 
 router.post("/member/signup", userController.userSingUp);
 
@@ -11,5 +12,10 @@ router.post("/product", productController.addProduct);
 router.delete("/product/:productId", productController.deleteProduct);
 router.put("/product/:productId", productController.updateProduct);
 router.get("/products", productController.getProducts);
+router.post(
+  "/upload/image",
+  uploadController.upload.single("file"),
+  uploadController.uploadImage
+);
 
 module.exports = router;
